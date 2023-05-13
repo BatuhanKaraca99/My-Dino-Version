a6a47eed1de0497f69fd0ce40d7b8e2644e2a72e
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-05-13 21:49:09 +0300

    Jumping
    with character controller component

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController character;
+    private Vector3 direction;
+
+    public float gravity = 9.81f * 2f;
+    public float jumpForce = 8f;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        direction = Vector3.zero; //reset direction
+    }
+
+    private void Update()
+    {
+        direction += Vector3.down * gravity * Time.deltaTime;
+
+        if (character.isGrounded) //built-in
+        {
+            direction = Vector3.down;
+
+            if (Input.GetButton("Jump")) //Hold down
+            {
+                direction = Vector3.up * jumpForce;
+            }
+        }
+
+        character.Move(direction * Time.deltaTime); //built-in
+    }
+}
