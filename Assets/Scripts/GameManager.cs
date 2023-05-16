118427d2aef986d876f9c97cc905592b94c76a62
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-05-16 20:54:08 +0300

    UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro; //Text Mesh Pro
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,6 +9,9 @@ public class GameManager : MonoBehaviour
     public float initialGameSpeed=5f;
     public float gameSpeedIncrease = 0.1f;
     public float gameSpeed { get; private set; }
+
+    public TextMeshProUGUI gameOverText;
+    public Button retryButton;
 
     private Player player;
     private Spawner spawner;
@@ -38,7 +43,7 @@ public class GameManager : MonoBehaviour
         NewGame();
     }
 
-    private void NewGame()
+    public void NewGame()
     {
         Obstacle[] obstacles = FindObjectsOfType<Obstacle>(); //to clean obstacles in game
 
@@ -52,6 +57,8 @@ public class GameManager : MonoBehaviour
 
         player.gameObject.SetActive(true);
         spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
     }
 
     public void GameOver()
@@ -61,6 +68,8 @@ public class GameManager : MonoBehaviour
 
         player.gameObject.SetActive(false);
         spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
     }
 
     private void Update()
